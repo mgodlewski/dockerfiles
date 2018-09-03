265b007a30d7e74e7531d20e447f5628a02a678b
Mathieu POTIER <mathieu.potier@onzeway.eu>
2018-09-03 23:10:21 +0200

    Fix typo

--- diacamma/entrypoint.d/sub_command/restore.sh
+++ diacamma/entrypoint.d/sub_command/restore.sh
@@ -5,7 +5,7 @@ function run {
     filename="/backups/${1}"
     if [ -f ${filename} ]
     then
-        /bin/bash launch_lucterios.sh archive --name=${CURRENT_ORGANISATION} --file=${filename}
+        /bin/bash launch_lucterios.sh restore --name=${CURRENT_ORGANISATION} --file=${filename}
     else
         echo "File ${filename} not found : cannot restore instance ${CURRENT_ORGANISATION}"
     fi
